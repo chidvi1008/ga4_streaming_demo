5759e195feab4b68dfaa2432744744d879af91d3
chidvi1008 <89209244+chidvi1008@users.noreply.github.com>
2025-10-18 20:43:33 +0800

    Refactor SQL for incremental event processing

--- models/int/int_events.sql
+++ models/int/int_events.sql
@@ -1,24 +1,41 @@
--- models/int/int_events.sql
 {{ config(
     materialized='incremental',
     unique_key='event_id'
 ) }}
 
 with new_events as (
-  select *
-  from `sylvan-client-475418-b8.realtime_demo.stg_events`
-  {% if is_incremental() %}
+    select *
+    from `realtime_demo.stg_events`
+    {% if is_incremental() %}
     where ingestion_time > (
-      select coalesce(max(ingestion_time), timestamp_sub(current_timestamp(), interval 24 hour))
-      from {{ this }}
+        select coalesce(max(ingestion_time), timestamp_sub(current_timestamp(), interval 24 hour))
+        from {{ this }}
     )
-  {% endif %}
-)
+    {% endif %}
+),
 
-select *(except rn)
-from (
-    select *,
-           row_number() over(partition by event_id order by ingestion_time desc) as rn
+ranked as (
+    select
+        event_id,
+        user_id,
+        event_name,
+        source,
+        medium,
+        campaign,
+        event_timestamp,
+        ingestion_time,
+        row_number() over(partition by event_id order by ingestion_time desc) as rn
     from new_events
 )
+
+select
+    event_id,
+    user_id,
+    event_name,
+    source,
+    medium,
+    campaign,
+    event_timestamp,
+    ingestion_time
+from ranked
 where rn = 1
